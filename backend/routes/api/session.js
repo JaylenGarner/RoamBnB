e1fa43747f828289e0194eff6113f404b57195b3
Jaylen Garner <jaylen.garner@outlook.com>
2022-08-15 19:36:38 -0400

    Add User input validation on user signup backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -42,30 +42,6 @@ router.post(
   }
 );
 
-
-router.post(
-  '/',
-  async (req, res, next) => {
-    const { credential, password } = req.body;
-
-    const user = await User.login({ credential, password });
-
-    if (!user) {
-      const err = new Error('Login failed');
-      err.status = 401;
-      err.title = 'Login failed';
-      err.errors = ['The provided credentials were invalid.'];
-      return next(err);
-    }
-
-    await setTokenCookie(res, user);
-
-    return res.json({
-      user
-    });
-  }
-);
-
 // Log out
 router.delete(
   '/',
