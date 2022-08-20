dae690e687202a856e9caa9a389908a250bbe08b
Jaylen Garner <jaylen.garner@outlook.com>
2022-08-20 15:30:30 -0400

    Get all of the current user's reviews

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,6 +3,9 @@ const express = require('express');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const { Spot } = require('../../db/models');
+const { Booking } = require('../../db/models');
+const { Image } = require('../../db/models');
+const { Review } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
@@ -102,6 +105,69 @@ router.get('/spots', restoreUser, async (req, res) => {
   })
 
   res.json(spots)
+})
+
+// Get all of the current users bookings
+router.get('/bookings', restoreUser, async (req, res) => {
+  const { user } = req;
+  const bookings = await Booking.findAll({
+    where: {
+      userId: user.id
+    },
+    attributes: [
+      'id', 'spotId', 'userId', 'startDate', 'endDate', 'createdAt', 'updatedAt'
+    ],
+
+    include: [
+      {
+      model: Spot,
+      attributes: [
+        'id', 'ownerId', 'address', 'city', 'state', 'country', 'lat',
+      'lng', 'name', 'price', 'previewImage'
+      ]
+    }]
+  })
+
+  res.json(bookings)
+})
+
+// Get all of the current users reviews
+router.get('/reviews', restoreUser, async (req, res) => {
+  const { user } = req;
+  const reviews = await Review.findAll({
+    where: {
+      userId: user.id
+    },
+    attributes: [
+      'id', 'userId', 'spotId', 'review', 'stars', 'createdAt', 'updatedAt'
+    ],
+
+    include: [
+      {
+      model: User,
+      attributes: [
+        'id', 'firstName','lastName'
+      ]
+    },
+    {
+      model: Spot,
+      attributes: [
+        'id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price'
+      ]
+    },
+    {
+      model: Image,
+      where: {
+        imageableType: 'review'
+      },
+      attributes: [
+        'id', 'imageableId', 'url'
+      ]
+    },
+  ]
+  })
+
+  res.json(reviews)
 })
 
 module.exports = router;
