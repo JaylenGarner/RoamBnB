c34db3ce5e52a2569945cf98b8cc83fb1e39fb47
Jaylen Garner <jaylen.garner@outlook.com>
2023-02-18 16:07:40 -0500

    User seed file adjusments for Render deployment

--- backend/db/seeders/20220813181342-demo-user.js
+++ backend/db/seeders/20220813181342-demo-user.js
@@ -1,9 +1,15 @@
 'use strict';
 const bcrypt = require("bcryptjs");
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', [
+    options.tableName = 'Users';
+    return queryInterface.bulkInsert(options, [
       {
         email: 'demo@user.io',
         hashedPassword: bcrypt.hashSync('password'),
@@ -27,8 +33,7 @@ module.exports = {
 
   down: async (queryInterface, Sequelize) => {
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete('Users', {
-      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
-    }, {});
+    options.tableName = 'Users';
+    return queryInterface.bulkDelete(options);
   }
 };
