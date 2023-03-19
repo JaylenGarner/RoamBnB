9712f873df7e148ad0242a7dcd3949da899851a9
Jaylen Garner <jaylen.garner@outlook.com>
2023-03-18 20:50:23 -0400

    Added meaningful seeder data

--- backend/db/seeders/20220813181342-demo-user.js
+++ backend/db/seeders/20220813181342-demo-user.js
@@ -11,25 +11,25 @@ module.exports = {
     options.tableName = 'Users';
     return queryInterface.bulkInsert(options, [
       {
-        email: 'demo@user.io',
+        email: 'demo@aa.io',
         hashedPassword: bcrypt.hashSync('password'),
-        firstName:'John',
-        lastName: 'Doe',
-        image: 'https://images.unsplash.com/photo-1564564244660-5d73c057f2d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2952&q=80'
+        firstName:'Demo',
+        lastName: 'User',
+        image: 'https://emojis.wiki/thumbs/emojis/man-technologist.webp'
       },
       {
-        email: 'user1@user.io',
-        hashedPassword: bcrypt.hashSync('password2'),
-        firstName: 'Tony',
-        lastName: 'Stark',
-        image: 'https://cdn.vox-cdn.com/thumbor/5bj7tnm3u0Qkndby6zwV5yuAY2U=/0x0:3000x1779/1820x1213/filters:focal(1204x216:1684x696):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/59606327/ktokatitmir0.0.jpg'
+        email: 'ben@aa.io',
+        hashedPassword: bcrypt.hashSync('password'),
+        firstName: 'Ben',
+        lastName: 'Goodman',
+        image: 'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80'
       },
       {
-        email: 'user2@user.io',
-        hashedPassword: bcrypt.hashSync('password3'),
-        firstName: 'Bill',
-        lastName: 'Burr',
-        image: 'https://www.hollywoodreporter.com/wp-content/uploads/2022/04/GettyImages-949563862-Splash-2022.jpg?w=2000&h=1126&crop=1'
+        email: 'jen@aa.io',
+        hashedPassword: bcrypt.hashSync('password'),
+        firstName: 'Jen',
+        lastName: 'Fryer',
+        image: 'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
       }
     ], {});
   },
--- backend/db/seeders/20220816230535-review-seeds.js
+++ backend/db/seeders/20220816230535-review-seeds.js
@@ -4,35 +4,77 @@ const { Review } = require('../models');
 
 let reviewSeeds = [
   {
-    userId: 1,
-    spotId: 2,
-    review: 'amazing place',
+    userId: 2,
+    spotId: 1,
+    review: "I just had the most amazing stay at this treehouse in the countryside. It was like a little slice of heaven up there! The farm animals were a nice touch, and the view was breathtaking. If you're looking for a peaceful getaway, this is the place to be.",
     stars: 5
   },
   {
-    userId: 2,
-    spotId: 3,
-    review: 'great getaway',
-    stars: 4
-  },
-  {
-    userId: 2,
-    spotId: 1,
-    review: 'great getaway',
-    stars: 3
-  },
-  {
     userId: 3,
     spotId: 1,
-    review: 'great getaway!',
+    review: "Loved this heavenly treehouse in the countryside! The view was breathtaking and it was the perfect peaceful retreat.",
     stars: 4
   },
   {
     userId: 3,
     spotId: 2,
-    review: 'great getaway!!!',
+    review: "Interesting stay at Moose Meadow Lodge Treehouse. Rustic charm, lovely natural setting, but lacking modern comforts. Overall, okay.",
     stars: 3
-  }
+  },
+  {
+    userId: 2,
+    spotId: 2,
+    review: "Interesting stay at the Moose Meadow Lodge Treehouse. Unique and well-crafted, but a bit smaller than expected. Beautiful nature surroundings.",
+    stars: 3
+  },
+  {
+    userId: 1,
+    spotId: 3,
+    review: 'The view from the top floor was something else - I swear, I could see all the way to Staten Island. And the amenities? Fuhgeddaboudit!',
+    stars: 5
+  },
+  {
+    userId: 3,
+    spotId: 3,
+    review: "Stunning view of Empire State Building, modern and clean place with everything needed. Perfect location, unforgettable experience!",
+    stars: 4
+  },
+  {
+    userId: 3,
+    spotId: 4,
+    review: 'Not a camping person, but okay stay at Airstream camper in Prince Edward County. Clean, comfortable, quiet setting. Different experience, but location was great with plenty to do. Worth trying for camping fans.',
+    stars: 3
+  },
+  {
+    userId: 1,
+    spotId: 4,
+    review: "Great retreat at Airstream camper in Prince Edward County. Cozy and peaceful with a unique setting. Perfect location for exploring. I even did some programming while camping! Highly recommended for a refreshing getaway.",
+    stars: 4
+  },
+  {
+    userId: 1,
+    spotId: 5,
+    review: "Absolutely loved the lake house near Jiminy Peak in Averill Park! The place was cozy and comfortable, and the surroundings were so peaceful and beautiful. It was the perfect escape from the city for a few days. I can't wait to come back and enjoy this lovely retreat again.",
+    stars: 4
+  },
+  {
+    userId: 2,
+    spotId: 5,
+    review: "Great family vacation at the cozy lake house near Jiminy Peak in Averill Park. Spacious, clean, and had everything we needed. Perfect location for outdoor activities. Hosts were wonderful. Highly recommend!",
+    stars: 4
+  },
+  {
+    userId: 2,
+    spotId: 6,
+    review: "The beach house was a real treat for this desert-dweller. Loved the sound of the waves, fresh sea air, and the town's local seafood. Can't wait to come back!",
+    stars: 5
+  },
+  {
+    userId: 1,
+    spotId: 6,
+    review: "Loved the beach house in East Lyme! Stunning views and beautiful decor. Perfect place to relax and explore the charming town. Highly recommend!",
+    stars: 4
+  },
 ]
 
 module.exports = {
