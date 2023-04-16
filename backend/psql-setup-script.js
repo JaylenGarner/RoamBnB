9d9b9cb12402648cf2e3ba0fafe294c8b68f9936
Jaylen Garner <jaylen.garner@outlook.com>
2023-04-16 14:22:03 -0400

    Continued render deployment troubleshooting

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,5 +1,7 @@
 const { sequelize } = require('./db/models');
 
+require('dotenv').config();
+
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
   if (!data.includes(process.env.SCHEMA)) {
     await sequelize.createSchema(process.env.SCHEMA);
